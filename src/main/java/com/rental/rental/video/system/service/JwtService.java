f66462f96743443a2f0930479f8e7fbb9773207d
Sujeet Krithik <79100419+sujeetkrithik@users.noreply.github.com>
2026-02-28 00:29:15 +0530

    Update JwtService.java

--- src/main/java/com/rental/rental/video/system/service/JwtService.java
+++ src/main/java/com/rental/rental/video/system/service/JwtService.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
